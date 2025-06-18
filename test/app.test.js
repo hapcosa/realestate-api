e8aca62c2559b123833dff2de8882d708fc99139
hapcosa <hapcosa@gmail.com>
2025-06-17 22:27:54 -0400

    actualizacion work v8

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../src/app');
 describe('GET /api/inmuebles', () => {
 it('debería retornar una lista de inmuebles', async () => {
-const res = await request(app).get('/api/inmuebles');
+const res = await request(app).get('/api/inmuebless');
 expect(res.statusCode).toBe(200);
 expect(res.body.length).toBeGreaterThan(0);
 expect(res.body[0]).toHaveProperty('direccion');
